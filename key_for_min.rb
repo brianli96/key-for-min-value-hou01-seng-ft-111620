09c4ec17a5d1713a786870f10637d4bbffdd7cda
Brian Li <brian.li96@yahoo.com>
2020-11-07 23:42:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     lowestValue = name_hash[]
     name_hash.each do |key, value|
       if lowestValue === 0
-        puts lowestValue = name_hash[:key]
+        puts name_hash[:key]
     end
   end
 end
